0cf29cb0f4478c73df5abda6d3b82e362472768e
shuan <tony85072700@gmail.com>
2023-02-20 22:51:59 +0800

    新增新北市及簡化程式碼

--- bot/views.py
+++ bot/views.py
@@ -9,7 +9,7 @@ import random
 
 from linebot import LineBotApi, WebhookHandler, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage, TextMessage, ImageSendMessage, StickerSendMessage, LocationSendMessage
+from linebot.models import MessageEvent, TextSendMessage, TextMessage, LocationSendMessage
 
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
@@ -24,8 +24,10 @@ def index(requests):
 def callback(request):
     keywords = {'words': ['哪條街或周邊景點呢？', '再請您輸入地址or景點！！',
                           '越詳細地址越好唷，前面不用加台北市唷～', '快給我其他資訊!要不然怎麼幫你找???', '今天想要減肥嗎，給我更我多資訊幫你找哪裡有ubike唷！！'],
-                'area': [
-                    '中山區，中正區，信義區，內湖區，\n北投區，南港區，士林區，大同區，\n大安區，文山區，松山區，臺大公館校區，\n臺大專區，萬華區', '想要找哪個區域呢？？']
+                '台北市': [
+                    '中山區，中正區，信義區，內湖區，\n北投區，南港區，士林區，大同區，\n大安區，文山區，松山區，臺大公館校區，\n臺大專區，萬華區', '想要找哪個區域呢？？'],
+                '新北市': [
+                    '瑞芳區，三芝區，金山區，深坑區，\n三峽區，鶯歌區，淡水區，永和區，\n泰山區，板橋區，中和區，樹林區，\n萬里區，五股區，八里區，汐止區，\n石門區，石碇區，土城區，新莊區，\n三重區，坪林區，新店區，蘆洲區，\n林口區，猴雙公共自行車專區', '想要找哪個區域呢？？']
                 }
 
     if request.method == 'POST':
@@ -43,81 +45,36 @@ def callback(request):
                     text = event.message.text
                     if '使用' in text or '說明' in text:
                         sendText(
-                            '歡迎使用ubike bot！\n查找規則:\n欲查找站點請先輸入區域(輸入區域即可查看)，不需要打"台北市" ，再輸入欲查找站點即可查詢\n完感謝您的使用！！', event)
-                    elif '區域' in text:
+                            '歡迎使用ubike bot！\n查找規則:\n欲查找站點請先輸入區域\n(輸入台北 或 新北 即可查看) \n，再輸入欲查找站點即可查詢\n完感謝您的使用！！', event)
+                    elif text == '台北市' or text == '臺北市' or text == '臺北' or text == '台北':
                         messages = [TextSendMessage(text)
-                                    for text in keywords['area']]
+                                    for text in keywords['台北市']]
                         line_bot_api.reply_message(event.reply_token, messages)
-                    elif text == '中山區':
-                        send_data = get_area(text)
-                        message = random.choice(keywords['words'])
-                        sendText(
-                            f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
-                    elif text == '中正區':
-                        send_data = get_area(text)
-                        message = random.choice(keywords['words'])
-                        sendText(
-                            f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
-                    elif text == '信義區':
-                        send_data = get_area(text)
-                        message = random.choice(keywords['words'])
-                        sendText(
-                            f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
-                    elif text == '內湖區':
-                        send_data = get_area(text)
-                        message = random.choice(keywords['words'])
-                        sendText(
-                            f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
-                    elif text == '北投區':
-                        send_data = get_area(text)
-                        message = random.choice(keywords['words'])
-                        sendText(
-                            f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
-                    elif text == '南港區':
-                        send_data = get_area(text)
-                        message = random.choice(keywords['words'])
-                        sendText(
-                            f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
-                    elif text == '士林區':
-                        send_data = get_area(text)
-                        message = random.choice(keywords['words'])
-                        sendText(
-                            f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
-                    elif text == '大同區':
-                        send_data = get_area(text)
-                        message = random.choice(keywords['words'])
-                        sendText(
-                            f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
-                    elif text == '大安區':
-                        send_data = get_area(text)
-                        message = random.choice(keywords['words'])
-                        sendText(
-                            f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
-                    elif text == '文山區':
-                        send_data = get_area(text)
-                        message = random.choice(keywords['words'])
-                        sendText(
-                            f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
-                    elif text == '松山區':
-                        send_data = get_area(text)
-                        message = random.choice(keywords['words'])
-                        sendText(
-                            f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
-                    elif '臺大專區' in text or '台大專區' in text or text == '臺大專區':
-                        send_data = get_area(text)
-                        message = random.choice(keywords['words'])
-                        sendText(
-                            f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
-                    elif '臺大公館校區' in text or '公館校區' in text:
-                        send_data = get_area(text)
-                        messages = LocationSendMessage(
-                            send_data[0][0], send_data[0][1], send_data[0][2], send_data[0][3]), TextSendMessage(f'更新時間：{send_data[0][6]} 目前車輛數量：{send_data[0][4]} 空位數量：{send_data[0][5]}')
+                    elif text == '新北市' or text == '新北':
+                        messages = [TextSendMessage(text)
+                                    for text in keywords['新北市']]
                         line_bot_api.reply_message(event.reply_token, messages)
-                    elif text == '萬華區':
-                        send_data = get_area(text)
+                    elif text in keywords['新北市'][0].replace(
+                            '\n', '').replace('，', ','):
+                        send_data = get_area(text, new_taipei=True)
                         message = random.choice(keywords['words'])
                         sendText(
                             f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
+
+                    elif text in keywords['台北市'][0].replace(
+                            '\n', '').replace('，', ','):
+                        if '臺大公館校區' in text or '公館校區' in text:
+                            send_data = get_area(text)
+                            messages = LocationSendMessage(
+                                send_data[0][0], send_data[0][1], send_data[0][2], send_data[0][3]), TextSendMessage(f'更新時間：{send_data[0][6]} 目前車輛數量：{send_data[0][4]} 空位數量：{send_data[0][5]}')
+                            line_bot_api.reply_message(
+                                event.reply_token, messages)
+                        else:
+                            send_data = get_area(text)
+                            message = random.choice(keywords['words'])
+                            sendText(
+                                f'{text}共有 {len(send_data)} 個站點 \n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n{message}', event)
+
                     else:
                         ai(text, event, line_bot_api)
 
@@ -134,26 +91,6 @@ def sendText(message, event):
     line_bot_api.reply_message(event.reply_token, message)
 
 
-def sendImage(img_url, event):
-    try:
-        message = ImageSendMessage(original_content_url=img_url,
-                                   preview_image_url=img_url)
-        line_bot_api.reply_message(event.reply_token, message)
-    except:
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text='傳送失敗!'))
-
-
-def sendSticker(p_id, s_id, event):
-    try:
-        message = StickerSendMessage(package_id=p_id,
-                                     sticker_id=s_id)
-        line_bot_api.reply_message(event.reply_token, message)
-    except:
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text='傳送失敗!'))
-
-
 def sendLocation(sna, ar, lat, lng, event):
     try:
         message = LocationSendMessage(title=sna,
@@ -166,15 +103,21 @@ def sendLocation(sna, ar, lat, lng, event):
             event.reply_token, TextSendMessage(text='傳送失敗!'))
 
 
-def analyze_area_data():
-    api_url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
+def analyze_area_data(new_taipei_area=False):
+    if new_taipei_area:
+        api_url = 'https://data.ntpc.gov.tw/api/datasets/010E5B15-3823-4B20-B401-B1CF000550C5/json?page=0&size=1000'
+    else:
+        api_url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
     df = pd.read_json(api_url)
     df1 = df[df['act'] == 1]
     columns = 'sarea sna ar lat lng tot sbi bemp mday'.split()
     ubike_data = df1[columns]
     ubike_data = ubike_data.rename(columns={
                                    'sarea': '區域', 'tot': '總車位數', 'sbi': '目前車輛數量', 'bemp': '空位數量', 'mday': '更新時間'})
-
+    if new_taipei_area:
+        ubike_data['更新時間'] = ubike_data['更新時間'].apply(lambda x: (str(x)))
+        ubike_data['更新時間'] = ubike_data['更新時間'].astype(
+            'datetime64[ns]').astype(str)
     ubike_data = ubike_data.sort_values('目前車輛數量', ascending=False)
     datas = ubike_data.values.tolist()
     return datas
@@ -199,8 +142,11 @@ def get_data(datas, name, area_dict):
     return send_data
 
 
-def get_area(text):
-    datas = analyze_area_data()
+def get_area(text, new_taipei=False):
+    if new_taipei:
+        datas = analyze_area_data(new_taipei_area=True)
+    else:
+        datas = analyze_area_data()
     area_dict = {}
     name = text
     send_data = get_data(datas, name, area_dict)
