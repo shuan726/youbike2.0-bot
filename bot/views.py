20132aca7628e0b15ef99f5b9885b0090261c8b4
shuan <tony85072700@gmail.com>
2023-02-20 18:23:51 +0800

    優化及新增文字說明

--- bot/views.py
+++ bot/views.py
@@ -23,7 +23,7 @@ def index(requests):
 @csrf_exempt
 def callback(request):
     keywords = {'words': ['哪條街或周邊景點呢？', '再請您輸入地址or景點！！',
-                          '越詳細地址越好唷，前面不用加台北市唷～', '快給我地址!要不然怎麼幫你找？？？', '今天想要減肥嗎，來騎ubike吧！！'],
+                          '越詳細地址越好唷，前面不用加台北市唷～', '快給我其他資訊!要不然怎麼幫你找？？？', '今天想要減肥嗎，給我更我多資訊幫你找哪裡有ubike唷！！'],
                 'area': [
                     '中山區，中正區，信義區，內湖區，\n北投區，南港區，士林區，大同區，\n大安區，文山區，松山區，臺大公館校區，\n臺大專區，萬華區', '想要找哪個區域呢？？']
                 }
@@ -43,7 +43,7 @@ def callback(request):
                     text = event.message.text
                     if '使用' in text or '說明' in text:
                         sendText(
-                            '歡迎使用"台北市" ubike bot！\n查找規則:\n欲查找站點請先輸入區域(輸入區域即可查看)，再輸入欲查找站點即可\n感謝您的使用！！', event)
+                            '歡迎使用ubike bot！\n查找規則:\n欲查找站點請先輸入區域(輸入區域即可查看)，不需要打"台北市" ，再輸入欲查找站點即可查詢\n如不確定是在哪裡，可以在輸入完區域後再輸入 "街" or "路" \n完感謝您的使用！！', event)
                     elif '區域' in text:
                         messages = [TextSendMessage(text)
                                     for text in keywords['area']]
@@ -305,7 +305,7 @@ def ai(text, event, line_bot_api):
             data2 = [data for data in send_data if text in data[0]
                      or text in data[1]]
             if len(data2) == 0:
-                messages = (TextSendMessage('您所找的地方/景點未搜尋到，請確認後再重新輸入！'))
+                messages = (TextSendMessage('您所找的地方/景點未搜尋到此站點名稱，請確認後再重新輸入！'))
             elif len(data2) == 1:
                 messages = LocationSendMessage(
                     data2[0][0], data2[0][1], data2[0][2], data2[0][3]), TextSendMessage(f'{data2[0][0]}\n 更新時間：{data2[0][6]} \n目前車輛數量：{data2[0][4]} 空位數量：{data2[0][5]}')
@@ -313,17 +313,13 @@ def ai(text, event, line_bot_api):
                 messages = LocationSendMessage(
                     data2[0][0], data2[0][1], data2[0][2], data2[0][3]), TextSendMessage(f'{data2[0][0]}\n 更新時間：{data2[0][6]} \n目前車輛數量：{data2[0][4]} 空位數量：{data2[0][5]}'), LocationSendMessage(
                     data2[1][0], data2[1][1], data2[1][2], data2[1][3]), TextSendMessage(f'{data2[1][0]}\n 更新時間：{data2[1][6]} \n目前車輛數量：{data2[1][4]} 空位數量：{data2[1][5]}')
-            elif len(data2) == 3:
-                messages = LocationSendMessage(
-                    data2[0][0], data2[0][1], data2[0][2], data2[0][3]), TextSendMessage(f'{data2[0][0]}\n 更新時間：{data2[0][6]} \n目前車輛數量：{data2[0][4]} 空位數量：{data2[0][5]}'), LocationSendMessage(
-                    data2[1][0], data2[1][1], data2[1][2], data2[1][3]), TextSendMessage(f'{data2[1][0]}\n 更新時間：{data2[1][6]} \n目前車輛數量：{data2[1][4]} 空位數量：{data2[1][5]}'), LocationSendMessage(
-                    data2[2][0], data2[2][1], data2[2][2], data2[2][3])
-            elif len(data2) >= 4:
-                messages = LocationSendMessage(
-                    data2[0][0], data2[0][1], data2[0][2], data2[0][3]), TextSendMessage(f'{data2[0][0]}\n 更新時間：{data2[0][6]} \n目前車輛數量：{data2[0][4]} 空位數量：{data2[0][5]}'), LocationSendMessage(
-                    data2[1][0], data2[1][1], data2[1][2], data2[1][3]), LocationSendMessage(
-                    data2[2][0], data2[2][1], data2[2][2], data2[2][3]), LocationSendMessage(
-                    data2[3][0], data2[3][1], data2[3][2], data2[3][3])
+            else:
+                data3 = [
+                    f'*您所查詢的: {text} 共有{len(data2)}個站點', f'*欲想查詢地圖位置，可輸入該站點名稱(如:{data2[0][0][11:]}，{data2[1][0][11:]}...)！！']
+                for i in data2:
+                    data3.append(i[0] + '\n' + ' 目前車輛數量為 :' +
+                                 str(i[-3]) + ' 空位數量為 : ' + str(i[-2]))
+                messages = TextSendMessage('\n\n'.join(data3))
             line_bot_api.reply_message(event.reply_token, messages)
             return
     except:
